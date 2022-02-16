7047699a315250133822b0d093fafad52532e75d
Kartikey Gupta <kartikeyrajgupta007@gmail.com>
2022-02-16 13:07:04 +0530

    updated

--- index.js
+++ index.js
@@ -29,11 +29,48 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
+                ${myLeads[i]}
+                
             </li>
         `
     }
     ulEl.innerHTML = listItems  
+}
+
+
+
+
+setInterval(showTime, 1000);
+function showTime() {
+	let time = new Date();
+	let hour = time.getHours();
+	let min = time.getMinutes();
+	let sec = time.getSeconds();
+	am_pm = "AM";
+
+	if (hour > 12) {
+		hour -= 12;
+		am_pm = "PM";
+	}
+	if (hour == 0) {
+		hr = 12;
+		am_pm = "AM";
+	}
+
+	hour = hour < 10 ? "0" + hour : hour;
+	min = min < 10 ? "0" + min : min;
+	sec = sec < 10 ? "0" + sec : sec;
+
+	let currentTime = hour + ":"
+			+ min + ":" + sec + am_pm;
+
+	document.getElementById("clock")
+			.innerHTML = currentTime;
+}
+showTime();
+
+if(showTime()===00){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
 }
